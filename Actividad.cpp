670f96d81eb427b55d9794b30c85523ee0c8517d
Manuel Zepeda <manolozr16@hotmail.com>
2023-10-16 10:03:02 -0600

    Comentarios de la función search

--- Actividad.cpp
+++ Actividad.cpp
@@ -252,25 +252,39 @@ vector<int> sais(vector<int> T) {
 
 //Search position of a string
 int search(const string& text, const string& pattern, const vector<int>& SA) {
+    // Initialize left and right pointers for binary search.
     int left = 0;
     int right = text.length() - 1;
+
+    // Initialize the variable to store the index of the first occurrence of the pattern.
     int first_occurrence = -1;
 
+    // Perform a binary search to find the pattern within the text.
     while (left <= right) {
+        // Calculate the middle index for the binary search.
         int mid = (left + right) / 2;
+
+        // Extract a suffix of the text starting from the SA[mid] position.
         string suffix = text.substr(SA[mid]);
+
+        // Compare the extracted suffix with the pattern.
         if (suffix.compare(0, pattern.length(), pattern) == 0) {
+            // If the pattern matches the suffix, update first_occurrence and continue searching to the left.
             first_occurrence = mid;
-            right = mid - 1;  // Continuar buscando a la izquierda
+            right = mid - 1;  // Continue with left searching
         } else if (pattern < suffix) {
+            // If the pattern is lexicographically less than the suffix, adjust the right pointer.
             right = mid - 1;
         } else {
+            // If the pattern is lexicographically greater than the suffix, adjust the left pointer.
             left = mid + 1;
         }
     }
 
+    // Return the index of the first occurrence of the pattern (or -1 if not found).
     return first_occurrence;
 }
+
 
 
 int main(int num, char* args[]) {
